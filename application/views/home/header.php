0e42f4a7882c9df26fdcbb799f9dad1f99ada109
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2017-11-19 19:43:01 +0700

    edit home and add image

--- application/views/home/header.php
+++ application/views/home/header.php
@@ -12,30 +12,10 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top navigasi">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><img src="<?=base_url('assets/img/profile/RP.png')?>" width="50" height="30" alt=""></a>
-          <a class="navbar-brand" href="#">Debting</a>
+          <a href="#"><img class="img-responsive" src="<?=base_url('assets/img/profile/debtingfix.png')?>" width="100" alt=""></a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right">
-            <?php if ($this->session->has_userdata('role')): ?>
-              <?php if ($this->session->userdata('role') =='admin'): ?>
-                <a href="<?=site_url('admin')?>" class="btn btn-success"><?=$this->session->userdata('role')?></a>
-              <?php else: ?>  
-                <a href="<?=site_url('debitur')?>" class="btn btn-success"><?=$this->session->userdata('role')?></a>
-              <?php endif; ?>
-            <?php else: ?>
-             <a href="<?=site_url('login')?>" class="btn btn-success">Login</a>
-            <?php endif ?>
-          </form>
-        </div><!--/.navbar-collapse -->
       </div>
     </nav>
